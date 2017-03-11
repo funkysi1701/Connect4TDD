6caa2806ffeb03d56df50c0428455a29959415b1
Simon Foster <funkysi1701@gmail.com>
2017-03-11 22:26:20 +0000

    change to run tests on Release code

--- coverage.bat
+++ coverage.bat
@@ -1,3 +1,3 @@
-packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe" -targetargs:"Connect4.Test\bin\Debug\Connect4.Test.dll" -output:"TestResults\coverage.xml" -register:user
+packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:"packages\NUnit.ConsoleRunner.3.6.1\tools\nunit3-console.exe" -targetargs:"Connect4.Test\bin\Release\Connect4.Test.dll" -output:"TestResults\coverage.xml" -register:user
 
 packages\ReportGenerator.2.5.6\tools\ReportGenerator.exe "-reports:TestResults\*.xml" "-targetdir:TestResults"
